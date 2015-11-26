4bb210b4d1af6909050e9ab86d76f2dd676433f6
Jay Dave <jaycdave@gmail.com>
2015-11-26 00:54:36 -0600

    changed entire css to become js

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+// cloud 1
+$(function() {
+    var img = $("#cloud_1"),
+        width = img.get(0).width,
+        screenWidth = $(window).width(),
+        duration = 15000;
+
+    function animatePlane() {
+        img.css("left", -width).animate({
+            "left": screenWidth
+        }, duration, animatePlane);
+    }
+
+    animatePlane();
+//cloud 2
+    var img = $("#cloud_2"),
+        width = img.get(0).width,
+        screenWidth = $(window).width(),
+        duration = 25000;
+
+    function animatePlane() {
+        img.css("left", -width).animate({
+            "left": screenWidth
+        }, duration, animatePlane);
+    }
+
+    animatePlane();
+
+
+
+
+
+});
+
